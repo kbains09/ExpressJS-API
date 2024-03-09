e9c57eb8d1bded3610233e5b68dd09ac92f4412f
Kirat Bains <kiratbains09@gmail.com>
2024-03-08 18:55:20 -0800

    feat: script update

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
-
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const { PORT, BASE_URL  } = process.env;
 const videosRouter = require('./routes/videos');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors());
+
+app.use((req, res, next)=>{
+  console.log('I am a middleware test');
+  next();
+});
+// middleware test, next() needed in middleware to move on to next function
 
 app.use('/videos', videosRouter);
 // app.use('/routes', videosRouter); sends the request to the videosRouter file, see if 'routes' can be removed to just '/'
@@ -12,3 +22,4 @@ app.use('/videos', videosRouter);
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
 });
+// tells express where to listen for server (.env holds more port info)
--- routes/videos.js
+++ routes/videos.js
@@ -1,20 +1,28 @@
-
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
 
 router.get('/', (req, res) => {
+  // const videoDataJSON = fs.readFileSync('./data/videos.json');
+  // const videoData = JSON.parse(videoDataJSON);
+  // console.log(videoData); 
+  // res.status(201).send(videoData);
   res.send('get request to the homepage');
 });
+// ^ test routes before fetching data from files 
 
-router.get('/videos', (req, res) => {
+router.get('/', (req, res) => {
   res.send({ message: 'Get request to /videos' });
 });
 
-router.get('/videos/:id', (req, res) => {
+router.get('/:id', (req, res) => {
+  // const videoId = req.params;
+  const { id } = req.params;
+  console.log(videoId);
   res.send({ message: 'Get request to specific video' });
 });
 
-router.post('/videos', (req, res) => {
+router.post('/', (req, res) => {
   res.send({ message: 'post request being sent', body: req.body });
   console.log(req.body);
   console.error(error);
