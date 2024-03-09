6cf70aacc460b52538261e9daee241da016cdf70
Kirat Bains <kiratbains09@gmail.com>
2024-03-08 19:09:18 -0800

    feat: cors dependency update

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
-const { PORT, BASE_URL  } = process.env;
+require('dotenv').config();
+const { PORT} = process.env;
 const videosRouter = require('./routes/videos');
 const cors = require('cors');
+const {CORS_ORIGIN} = process.env;
 
-app.use(express.json());
-app.use(express.static('public'));
-app.use(cors());
+console.log(process.env); 
+// testing enviroment variables
+console.log('I am a test message');
 
 app.use((req, res, next)=>{
   console.log('I am a middleware test');
@@ -15,11 +16,18 @@ app.use((req, res, next)=>{
 });
 // middleware test, next() needed in middleware to move on to next function
 
+// Middleware
+app.use((cors()));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cors());
+
+
 app.use('/videos', videosRouter);
 // app.use('/routes', videosRouter); sends the request to the videosRouter file, see if 'routes' can be removed to just '/'
 // directs express to the router with routes
 
-app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`listening at http://localhost:${PORT}`); 
 });
 // tells express where to listen for server (.env holds more port info)
