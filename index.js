f5d61e715a91590859b4f63278604c2ef6510fcc
Kirat Bains <kiratbains09@gmail.com>
2024-03-10 14:24:06 -0700

    feat: image hardcoding in data

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { PORT} = process.env;
 const videosRouter = require('./routes/videos');
 const cors = require('cors');
 const {CORS_ORIGIN} = process.env;
-
+const path = require('path');
 
 console.log(process.env); 
 // testing enviroment variables
@@ -15,7 +15,7 @@ console.log('I am a test message');
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/videos', videosRouter);
